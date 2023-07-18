943a48e91a548dad2d6cc6cb5ff31a778ef39ee7
Therakar <matteo.pau200@gmail.com>
2023-07-18 22:10:36 +0200

    use state to make this interactive

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 const faqs = [
@@ -18,28 +19,46 @@ const faqs = [
 export default function App() {
   return (
     <div>
+      {/* here I'm passing the faqs (array of objects) as 
+      a 'data' prop */}
       <Accordion data={faqs} />
     </div>
   );
 }
 
+//here I'm receiving the data prop from the father component
 function Accordion({ data }) {
   return (
     <div className="accordion">
+      {/* Using map() to  cycle through the array and for 
+      every element I create an <AccordionItem/>*/}
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} />
+        //Accordion items with props and the key
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
       ))}
     </div>
   );
 }
 
 function AccordionItem({ num, title, text }) {
+  //Piece of state | initial value is false so that the accordion elements are all close in the beginning
+  const [isOpen, setIsOpen] = useState(false);
+
+  //handler function
+  function handleToggle() {
+    //Given a curIsOpen (can be true or false) reverse it
+    setIsOpen((curIsOpen) => !isOpen);
+  }
+
+  //JSX
   return (
-    <div className="item">
+    <div className={`item ${isOpen ? "open" : ""} `} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {/* If isOpen = false doesn't show the div */}
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
